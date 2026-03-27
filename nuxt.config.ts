8e883c65120e948f0add6cfa21305e4fb8c18d80
PSolntsev <PSolntsev@synergy.ru>
2026-03-27 13:29:47 +0300

    Update favicon configuration in Nuxt to include a new SVG icon and modify its colors for improved branding consistency. This change enhances the visual identity of the site across different platforms.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     head: {
       // Подключаем фавиконки и PWA-иконки глобально для всех страниц сайта.
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
         { rel: 'shortcut icon', href: '/favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
